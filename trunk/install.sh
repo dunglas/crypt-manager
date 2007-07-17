8fb2bf47f9b00cbfd8a53e6f82d15f402d4eb348
dunglas <dunglas@6fcbdafe-e833-0410-a872-7d7b3ea282c5>
2007-07-17 11:14:48 +0000

    Added desktop file

--- trunk/install.sh
+++ trunk/install.sh
@@ -2,6 +2,7 @@
 mkdir /usr/share/gcrypt-manager/
 cp share/gcrypt-manager.glade /usr/share/gcrypt-manager/
 cp share/icon.png /usr/share/gcrypt-manager/
+cp share/gcrypt-manager.desktop /usr/share/applications/
 cp src/cryptmanager.py /usr/lib/python2.5/
 chmod a+rx src/crypt-manager src/gcrypt-manager
 cp src/crypt-manager src/gcrypt-manager /usr/bin
