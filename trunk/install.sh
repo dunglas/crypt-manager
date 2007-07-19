3effb7068d88f6ee6b824857162ccd465629bb38
dunglas <dunglas@6fcbdafe-e833-0410-a872-7d7b3ea282c5>
2007-07-19 15:24:52 +0000

    Improved encrypting, use rename instead of cp/rm, faster and don't need 2 * current disk usage

--- trunk/install.sh
+++ trunk/install.sh
@@ -6,4 +6,4 @@ cp share/gcrypt-manager.desktop /usr/share/applications/
 cp src/cryptmanager.py /usr/lib/python2.5/
 chmod a+rx src/crypt-manager src/gcrypt-manager
 cp src/crypt-manager src/gcrypt-manager /usr/bin
-cp nautilus-extension/crypt-manager.py /usr/lib/nautilus/extensions-1.0/python/
+cp src/nautilus-extension/crypt-manager.py /usr/lib/nautilus/extensions-1.0/python/
