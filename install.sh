5cca3f277ce171f3d7027773d959f51ea8d331cd
dunglas <dunglas@6fcbdafe-e833-0410-a872-7d7b3ea282c5>
2007-07-23 15:20:42 +0000

    Reorganisation of the repository

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir /usr/share/gcrypt-manager/
-cp share/gcrypt-manager.glade /usr/share/gcrypt-manager/
-cp share/icon.png /usr/share/gcrypt-manager/
-cp share/gcrypt-manager.desktop /usr/share/applications/
-cp src/cryptmanager.py /usr/lib/python2.5/
-chmod a+rx src/crypt-manager src/gcrypt-manager
-cp src/crypt-manager src/gcrypt-manager /usr/bin
-cp src/nautilus-extension/crypt-manager.py /usr/lib/nautilus/extensions-1.0/python/
+cp gcrypt-manager/share/gcrypt-manager.glade /usr/share/gcrypt-manager/
+cp gcrypt-manager/share/icon.png /usr/share/gcrypt-manager/
+cp gcrypt-manager/share/gcrypt-manager.desktop /usr/share/applications/
+cp python-cryptmanager/cryptmanager.py /usr/lib/python2.5/
+chmod a+rx crypt-manager/crypt-manager gcrypt-manager/gcrypt-manager
+cp crypt-manager/crypt-manager gcrypt-manager/gcrypt-manager /usr/bin
+cp nautilus-crypt/crypt-manager.py /usr/lib/nautilus/extensions-1.0/python/
