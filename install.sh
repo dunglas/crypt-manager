b0658b2bc8decb3585df8d032db5d077a171ef0f
dunglas <dunglas@6fcbdafe-e833-0410-a872-7d7b3ea282c5>
2007-07-23 17:31:17 +0000

    Debian packages

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mkdir /usr/share/gcrypt-manager/
 cp gcrypt-manager/share/gcrypt-manager.glade /usr/share/gcrypt-manager/
 cp gcrypt-manager/share/icon.png /usr/share/gcrypt-manager/
 cp gcrypt-manager/share/gcrypt-manager.desktop /usr/share/applications/
-cp python-cryptmanager/cryptmanager.py /usr/lib/python2.5/
+cp crypt-manager/cryptmanager.py /usr/lib/python2.5/
 chmod a+rx crypt-manager/crypt-manager gcrypt-manager/gcrypt-manager
 cp crypt-manager/crypt-manager gcrypt-manager/gcrypt-manager /usr/bin
 cp nautilus-crypt/crypt-manager.py /usr/lib/nautilus/extensions-1.0/python/
