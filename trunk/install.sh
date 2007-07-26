07d55dc53a037fec65c11818d728345a0e23c0bd
dunglas <dunglas@6fcbdafe-e833-0410-a872-7d7b3ea282c5>
2007-07-26 08:28:42 +0000

    Fix in nautilus-foldercrypt, cannot have the same name than the Python module, removed some useless 'print'

--- trunk/install.sh
+++ trunk/install.sh
@@ -6,4 +6,4 @@ cp foldercrypt-gtk/share/foldercrypt.desktop /usr/share/applications/
 cp foldercrypt/foldercrypt.py /usr/lib/python2.5/
 chmod a+rx foldercrypt/foldercrypt foldercrypt-gtk/foldercrypt-gtk
 cp foldercrypt/foldercrypt foldercrypt-gtk/foldercrypt-gtk /usr/bin
-cp nautilus-foldercrypt/foldercrypt.py /usr/lib/nautilus/extensions-1.0/python/
+cp nautilus-foldercrypt/crypt.py /usr/lib/nautilus/extensions-1.0/python/
