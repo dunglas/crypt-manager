5e9ea3f04744f8e3489a57f5b1df12cdcc2bd3d6
dunglas <dunglas@6fcbdafe-e833-0410-a872-7d7b3ea282c5>
2007-07-30 13:01:59 +0000

    Add support for opening encrypted folders in foldercrypt-kde

--- trunk/foldercrypt-kde/WindowOpen.py
+++ trunk/foldercrypt-kde/WindowOpen.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'qt3/WindowOpen.ui'
 #
-# Created: lun jui 30 14:40:14 2007
+# Created: lun jui 30 15:00:39 2007
 #      by: The PyQt User Interface Compiler (pyuic) 3.17
 #
 # WARNING! All changes made in this file will be lost!
@@ -21,9 +21,6 @@ class WindowOpen(QDialog):
         self.setSizeGripEnabled(1)
 
 
-        self.open_close = QCheckBox(self,"open_close")
-        self.open_close.setGeometry(QRect(10,100,130,21))
-
         LayoutWidget = QWidget(self,"Layout1")
         LayoutWidget.setGeometry(QRect(10,130,300,33))
         Layout1 = QHBoxLayout(LayoutWidget,0,6,"Layout1")
@@ -39,26 +36,29 @@ class WindowOpen(QDialog):
         self.open_cancel.setAutoDefault(1)
         Layout1.addWidget(self.open_cancel)
 
-        self.textLabel3 = QLabel(self,"textLabel3")
-        self.textLabel3.setGeometry(QRect(200,100,110,21))
-
-        self.open_path = QLabel(self,"open_path")
-        self.open_path.setGeometry(QRect(120,20,190,21))
-
         self.textLabel1_2 = QLabel(self,"textLabel1_2")
         self.textLabel1_2.setGeometry(QRect(10,60,91,21))
 
         self.textLabel1_2_2 = QLabel(self,"textLabel1_2_2")
         self.textLabel1_2_2.setGeometry(QRect(10,20,91,21))
 
-        self.crypt_password = QLineEdit(self,"crypt_password")
-        self.crypt_password.setGeometry(QRect(120,60,190,21))
-        self.crypt_password.setEchoMode(QLineEdit.Password)
+        self.textLabel3 = QLabel(self,"textLabel3")
+        self.textLabel3.setGeometry(QRect(200,100,110,21))
+
+        self.open_close = QCheckBox(self,"open_close")
+        self.open_close.setGeometry(QRect(10,100,130,21))
 
         self.open_time = QSpinBox(self,"open_time")
         self.open_time.setEnabled(0)
         self.open_time.setGeometry(QRect(140,100,51,21))
         self.open_time.setMinValue(1)
+
+        self.open_password = QLineEdit(self,"open_password")
+        self.open_password.setGeometry(QRect(120,60,190,21))
+        self.open_password.setEchoMode(QLineEdit.Password)
+
+        self.open_path = QLabel(self,"open_path")
+        self.open_path.setGeometry(QRect(120,20,190,21))
 
         self.languageChange()
 
@@ -68,15 +68,15 @@ class WindowOpen(QDialog):
 
     def languageChange(self):
         self.setCaption(self.__tr("Open an encrypted folder"))
-        self.open_close.setText(self.__tr("Auto close after"))
         self.open_ok.setText(self.__tr("&OK"))
         self.open_ok.setAccel(QKeySequence(QString.null))
         self.open_cancel.setText(self.__tr("&Cancel"))
         self.open_cancel.setAccel(QKeySequence(QString.null))
-        self.textLabel3.setText(self.__tr("minutes of IDLE"))
-        self.open_path.setText(QString.null)
         self.textLabel1_2.setText(self.__tr("Password:"))
         self.textLabel1_2_2.setText(self.__tr("Path:"))
+        self.textLabel3.setText(self.__tr("minutes of IDLE"))
+        self.open_close.setText(self.__tr("Auto close after"))
+        self.open_path.setText(QString.null)
 
 
     def __tr(self,s,c = None):
