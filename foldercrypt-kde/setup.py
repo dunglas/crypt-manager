8e80346c810f8a9fa5f65908022b036a2601622a
dunglas <dunglas@6fcbdafe-e833-0410-a872-7d7b3ea282c5>
2007-08-01 13:37:39 +0000

    Added setup.py (distutils installation script), moved Window* to ui package, extented support for KCModule

---
+++ foldercrypt-kde/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8 :
+# Copyright (C) 2007 Kévin Dunglas <dunglas@gmail.com>
+# Copyright (C) 2003-2006 by Simon Edwards <simon@simonzone.com>
+#
+# Authors:
+#  Kévin Dunglas
+#
+# This program is a part of a the Google Summer Of Code 2007
+# For futher information see :
+# http://code.google.com/soc/ubuntu/appinfo.html?csaid=EF4FCF874D88234
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# Setup script based on Guidance - http://www.simonzone.com/software/guidance/
+
+from distutils.core import Extension
+import kdedistutils
+
+def doit():
+    kdedistutils.setup(name="foldercrypt-kde",
+        version="0.0.1",
+        author="Kévin Dunglas",
+        author_email="dunglas@gmail.com",
+        url="http://code.google.com/p/crypt-manager/",
+        min_kde_version = "3.0.0",
+        min_qt_version = "3.0.0",
+        license = "GPL",
+        application_data = ["foldercrypt-kde.py"],
+        executable_links = [("foldercrypt-kde", "foldercrypt-kde.py")],
+        kcontrol_modules = [("foldercrypt-kde.desktop", "foldercrypt-kde")],
+
+        py_modules=["ui.WindowOpen", "ui.WindowCrypt", "ui.WindowDecrypt",\
+            "ui.WindowManager", "ui.WindowProperties", "ui.WindowError"])
+doit()
