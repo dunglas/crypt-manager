5355c9b2aff8bfa1e9925ded90a0b7dfbb15dec3
dunglas <dunglas@6fcbdafe-e833-0410-a872-7d7b3ea282c5>
2007-08-07 12:49:54 +0000

    Many many bug fixes in foldercrypt-gtk and the python module. New unit test script for foldercrypt-gtk

--- foldercrypt-gtk/test.sh
+++ foldercrypt-gtk/test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "Unit tests for foldercrypt"
 echo "-------------------------------------------------------------------------"
-echo "Command line tool"
+echo "GTK interface"
 echo "-------------------------------------------------------------------------"
-echo "Install the command line tool..."
+echo "Install the gtk interface..."
 sudo rm -rf build
 find . -name '*.py[co]' | xargs rm -f
 sudo python setup.py build
 sudo python setup.py install
-foldercrypt --version
-foldercrypt --help
+foldercrypt-gtk --version
+foldercrypt-gtk --info
 
 if [ -e dontexists ]
 then
@@ -17,15 +17,13 @@ then
 fi
 
 echo "Open a directory that don't exists..."
-foldercrypt --open dontexists
+foldercrypt-gtk --open dontexists
 echo "Encrypt a directory that don't exists..."
-foldercrypt --crypt dontexists
+foldercrypt-gtk --crypt dontexists
 echo "Close a directory that don't exists..."
-foldercrypt --close dontexists
+foldercrypt-gtk --close dontexists
 echo "Decrypt a directory that don't exists..."
-foldercrypt --decrypt dontexists
-echo "Change the password of a directory that don't exists..."
-foldercrypt --change-pass dontexists
+foldercrypt-gtk --decrypt dontexists
 
 echo "Create a directory with some files and subdirecotries and encrypt it..."
 
@@ -42,20 +40,16 @@ echo "test1" > testencrypt/haha
 echo "test2" > testencrypt/sub1/haha
 echo "test3" > testencrypt/sub2/plop
 echo "test4" > testencrypt/hohoooo
-foldercrypt --crypt testencrypt
+foldercrypt-gtk --crypt testencrypt
 echo "Close it..."
-foldercrypt --close testencrypt
-echo "Change password... Please enter a BAD password."
-foldercrypt --change-pass testencrypt
-echo "Change password... Please DON'T match confirmation and password."
-foldercrypt --change-pass testencrypt
-echo "Change password... Please enter a GOOD password."
-foldercrypt --change-pass testencrypt
+foldercrypt-gtk --close testencrypt
+
+
 echo "Open it... Please enter a BAD password."
-foldercrypt --open testencrypt
+foldercrypt-gtk --open testencrypt
 echo "Open it... Please enter the GOOD password."
-foldercrypt --open testencrypt
+foldercrypt-gtk --open testencrypt
 echo "Decrypt it... Please enter a BAD password."
-foldercrypt --decrypt testencrypt
+foldercrypt-gtk --decrypt testencrypt
 echo "Decrypt it... Please enter the GOOD password."
-foldercrypt --decrypt testencrypt
+foldercrypt-gtk --decrypt testencrypt
