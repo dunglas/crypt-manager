6e2d6efe8336c909f1c850e82b66ac9da862b617
dunglas <dunglas@6fcbdafe-e833-0410-a872-7d7b3ea282c5>
2007-08-24 12:37:22 +0000

    Fixed dependency problem in Debian package

--- make-debian.sh
+++ make-debian.sh
@@ -1,12 +1,9 @@
-#!bin/bash
-cd ../deb/
+#!/bin/bash
 rm -Rf *
 svn checkout http://crypt-manager.googlecode.com/svn/trunk/ crypt-manager
-cd crypt-manager
-bash clean-svn.sh
-cd ..
+cd crypt-manager && bash clean-svn.sh && cd ..
 mv crypt-manager/debian .
 mv crypt-manager conceal-0.0.5
 tar -czvf conceal_0.0.6.orig.tar.gz conceal-0.0.5
-cd conceal-0.0.5
-debuild -us -uc
+mv debian conceal-0.0.5
+cd conceal-0.0.5 && pdebuild 
