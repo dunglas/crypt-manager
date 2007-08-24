9adf0a8091e4a43b464ecd7f94e2436a515b29d1
dunglas <dunglas@6fcbdafe-e833-0410-a872-7d7b3ea282c5>
2007-08-24 08:46:04 +0000

    Fixes to the Debian package

--- trunk/make-debian.sh
+++ trunk/make-debian.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-cd deb/
+cd ../deb/
 rm -Rf *
 svn checkout http://crypt-manager.googlecode.com/svn/trunk/ crypt-manager
 cd crypt-manager
@@ -7,6 +7,6 @@ bash clean-svn.sh
 cd ..
 mv crypt-manager/debian .
 mv crypt-manager conceal-0.0.5
-tar -czvf conceal_0.0.5.orig.tar.gz conceal-0.0.5
+tar -czvf conceal_0.0.6.orig.tar.gz conceal-0.0.5
 cd conceal-0.0.5
 debuild -us -uc
